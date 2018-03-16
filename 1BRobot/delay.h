0f1b5d4a8e89f51868a4fd49d952b9e527e6daaa
Max Vigdorchik <max@vigdorchik.org>
2018-03-16 08:05:09 +0000

    Revert "Will's changes and changes from day before competition"
    This reverts commit a7fe482f8308afa3b9e2a0c35f06eedec4d3e6a9.

--- 1BRobot/delay.h
+++ 1BRobot/delay.h
@@ -1,10 +1,1 @@
-#ifndef _CUED_DELAY_INCLUDED
-#define _CUED_DELAY_INCLUDED
-
-#ifndef __cplusplus
-#error delay.h must be used from C++, not C code
-#endif
-
-void delay(int);
-
-#endif  /* _CUED_DELAY_INCLUDED */
+robot_delay.h
