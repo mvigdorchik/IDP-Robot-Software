a7fe482f8308afa3b9e2a0c35f06eedec4d3e6a9
M.D. Vigdorchik <mdv29@balmung.eng.cam.ac.uk>
2018-03-16 07:37:50 +0000

    Will's changes and changes from day before competition

--- 1BRobot/delay.h
+++ 1BRobot/delay.h
@@ -1,1 +1,10 @@
-robot_delay.h
+#ifndef _CUED_DELAY_INCLUDED
+#define _CUED_DELAY_INCLUDED
+
+#ifndef __cplusplus
+#error delay.h must be used from C++, not C code
+#endif
+
+void delay(int);
+
+#endif  /* _CUED_DELAY_INCLUDED */
